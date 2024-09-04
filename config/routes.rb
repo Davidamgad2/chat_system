29c10c8aae5e2ad3588398787441b1ace4d6c184
David Amgad <davidamgad2@icloud.com>
2024-09-04 04:37:31 +0300

    Refactor: configuration files modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
+  resources :applications, only: [:create, :show, :index, :update], param: :token do
+    resources :chats, param: :number, only: [:create, :index, :show] do
+      resources :messages, param: :number, only: [:create, :index]
+    end
+    get "chats/:chat_number/messages/search", to: "messages#search"
+  end
 end
